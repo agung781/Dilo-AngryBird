37fe5c29bd8e6316212ad6978a625d18b23dcb88
DESKTOP-7523RE5\Agung Wicaksono <initialaw781@gmail.com>
2021-05-04 16:18:45 +0700

    Initial
    Initial

---
+++ Assets/Script/OwlBird.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+
+    public float fieldimpact;
+    public float force;
+    public LayerMask layertohit;
+    public bool _hasexploade = false;
+
+    public void explode()
+    {
+        if (State == BirdState.Thrown && !_hasexploade)
+        {
+            Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, fieldimpact, layertohit);
+
+            foreach (Collider2D obj in objects)
+            {
+                Vector2 direction = obj.transform.position - transform.position;
+
+                obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
+            }
+            _hasexploade = true;
+        }
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, fieldimpact);
+    }
+
+    public override void OnTap()
+    {
+        explode();
+    }
+}
